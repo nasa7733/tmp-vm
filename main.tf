c61400936e015e0df6c82d919e16b2949b64cc67
nasa7733 <58895722+nasa7733@users.noreply.github.com>
2020-03-25 12:17:23 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "google" {
+  #credentials = file("terraform.json")
+  project = "lyrical-shore-262101"
+  region  = "us-central1"
+  
+}
+
+
+resource "google_compute_instance" "vm-instance" {
+  name         = "tmp-vm"
+  machine_type = "f1-micro"
+  zone         = "us-central1-c"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  metadata_startup_script = file("startup.sh")
+   
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
+© 2020 GitHub, Inc.
